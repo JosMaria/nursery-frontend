901d853b8ff73ca6f5f8be7c727023f866b4513b
Jose Maria <josmariaguilar@gmail.com>
2023-01-07 21:14:19 -0400

    refactor code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,48 +8,62 @@ export const Footer = () => {
     <footer className='footer-container'>
       
       <div className='footer-section-container'>
-        <div className='footer-section'>
-          <h2>Información</h2>
-          <br />
-          <p>
-            La Universidad Mayor de San Simón (UMSS), fundada por Ley del 5 de noviembre de 1832, durante la 
-            Presidencia del Mcal. Andrés de Santa Cruz, es una entidad autónoma, de derecho público, constituida 
-            por docentes, estudiantes y personal administrativo, con personería jurídica propia, reconocida por 
-            la Constitución Política del Estado. Cubre el área de la educación superior con sus funciones de 
-            enseñanza-aprendizaje, investigación científica y tecnológica e interacción social universitaria.
-          </p>
-        </div>
-        
-        <div className='footer-section'>
-          <h2>Sobre el vivero</h2>
-          <br />
-          <p>
-            En los viveros se mantienen condiciones especiales en lo que respecta a la intensidad de luz solar 
-            que incide sobre la planta, manteniendo un mínimo de ésta. Una vez que la plántula se ha desarrollado 
-            y ya es una planta desarrollada, se hace el trasplante definitivo a campo. El vivero es una práctica 
-            común en frutales. También es llamado vivero a las instalaciones en las cuales se comercia una gran 
-            cantidad de plantas, especialmente ornamentales
-          </p>
-        </div>
+        <FooterSection 
+          title='Información'
+          content='La Universidad Mayor de San Simón (UMSS), fundada por Ley del 5 de noviembre de 1832, durante la 
+          Presidencia del Mcal. Andrés de Santa Cruz, es una entidad autónoma, de derecho público, constituida 
+          por docentes, estudiantes y personal administrativo, con personería jurídica propia, reconocida por 
+          la Constitución Política del Estado. Cubre el área de la educación superior con sus funciones de 
+          enseñanza-aprendizaje, investigación científica y tecnológica e interacción social universitaria.'
+        />
 
-        <div className='footer-section'>
+        <FooterSection 
+          title='Sobre el vivero'
+          content='En los viveros se mantienen condiciones especiales en lo que respecta a la intensidad de luz solar 
+          que incide sobre la planta, manteniendo un mínimo de ésta. Una vez que la plántula se ha desarrollado 
+          y ya es una planta desarrollada, se hace el trasplante definitivo a campo. El vivero es una práctica 
+          común en frutales. También es llamado vivero a las instalaciones en las cuales se comercia una gran 
+          cantidad de plantas, especialmente ornamentales'
+        />
+
+        <FooterSectionReference />
+      </div>
+      <div>
+        section end
+      </div>
+    </footer>
+  )
+}
+
+interface FooterSectionInterface {
+  title: string,
+  content: string
+}
+
+const FooterSection = ({ title, content }: FooterSectionInterface) => (
+  <div className='footer-section'>
+    <h2>{title}</h2>
+    <br />
+    <p>{content}</p>
+  </div>
+)
+
+const FooterSectionReference = () => {
+  return (
+    <div className='footer-section'>
           <div className='footer-section-location-social'>
             <div className='footer-section-location'>
               <h2>Informacion de contacto</h2>
               <br />
-              <div style={{display: 'flex', alignItems: 'center'}}>
-                <div style={{paddingRight: '1em'}}>
-                  <IoLocationSharp size='1.5em' />
-                </div>
-                <p>Km5 - Avenida petrolera Facultad de ciencias Rurales y Territoriales</p>
-              </div>
-              <br />
-              <div style={{display: 'flex', alignItems: 'center'}}>
-                <div style={{paddingRight: '1em'}}>
-                  <FaPhoneAlt size='1.5em' />
-                </div>
-                <p>+(591) 79368354</p>
-              </div>
+              <InformationReference 
+                icon={<IoLocationSharp size='1.5em' />}
+                content='Km5 - Avenida petrolera Facultad de ciencias Rurales y Territoriales' 
+              />
+
+              <InformationReference 
+                icon={<FaPhoneAlt size='1.5em' />}
+                content='+(591) 79368354'
+              />
 
             </div>
             <div className='footer-icons-container'>
@@ -61,10 +75,21 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-      </div>
+  )
+}
+
+interface InformationReferenceInterface {
+  icon: JSX.Element
+  content: string
+}
+
+const InformationReference = ({ icon, content }: InformationReferenceInterface) => {
+  return (
+    <div className='information-reference-container'>
       <div>
-        section end
+        {icon}
       </div>
-    </footer>
+      <p>{content}</p>
+    </div>
   )
 }
