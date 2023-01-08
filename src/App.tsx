53f7665a2cc8f2c9984476e82ce8d41d051666fc
Jose Maria <josmariaguilar@gmail.com>
2023-01-08 13:29:38 -0400

    Set footer bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
-    <div>
+    <div  className='app-container'>
+      <Header />
+      <h1>CONTENT 1</h1>
+      <h1>CONTENT 2</h1>
+      <h1>CONTENT 3</h1>
+      <h1>CONTENT 4</h1>
+      <h1>CONTENT 5</h1>
+      <h1>CONTENT 6</h1>
+      <h1>CONTENT 7</h1>
+      <h1>CONTENT 8</h1>
+      <h1>CONTENT 9</h1>
+      <h1>CONTENT 10</h1>
+      <h1>CONTENT 11</h1>
+      <h1>CONTENT 12</h1>
+      <h1>CONTENT 13</h1>
+      <h1>CONTENT 14</h1>
+      <h1>CONTENT 15</h1>
+      <h1>CONTENT 16</h1>
+      <h1>CONTENT 17</h1>
+      <h1>CONTENT 18</h1>
+      <h1>CONTENT 19</h1>
+      <h1>CONTENT 20</h1>
+      <h1>CONTENT 21</h1>
+      <h1>CONTENT 22</h1>
+      <h1>CONTENT 23</h1>
+      <h1>CONTENT 24</h1>
+      <h1>CONTENT 25</h1>
+      <h1>CONTENT 26</h1>
+      <h1>CONTENT 27</h1>
+      <h1>CONTENT 28</h1>
       <Footer />      
     </div>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './stylesheets/Header.css'
+
+export const Header = () => {
+  return (
+    <header className='header-container'>
+      <h1>Header</h1>
+    </header>
+  )
+}
