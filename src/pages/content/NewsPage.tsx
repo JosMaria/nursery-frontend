259539827901ddaa54bd8c00bb5d4097d8b3367b
Jose Maria <josmariaguilar@gmail.com>
2023-01-30 00:23:29 -0400

    adding and applying tailwindcss

--- src/pages/content/NewsPage.tsx
+++ src/pages/content/NewsPage.tsx
@@ -1,12 +1,10 @@
 import { NewsResponseDTO } from '../../types';
-import {  news as myNews } from '../../utils';
+import { news as myNews } from '../../utils';
 import newsImage from '../../assets/news-picture.png'
-
-import './stylesheets/NewsPage.css'
 
 export const NewsPage = () => {
   return (
-    <div className='news-container'>
+    <div className='flex flex-col items-center bg-green-700'>
       {
         myNews.map(news => 
           <News 
@@ -23,16 +21,18 @@ export const NewsPage = () => {
 }
 
 const News = ({ title, contentToShow, content, urlImage }: NewsResponseDTO) => (
-  <article>
+  <article className='bg-lime-200 max-w-3xl rounded-lg flex items-center justify-evenly gap-0 p-4 my-4'>
     {
-      urlImage && <img src={newsImage} alt={title} className='news-image' /> 
+      urlImage && <img src={newsImage} alt={title} className='w-48 h-40' />
     }
-    
-    <div>
-      <h1>{title}</h1>
-      <p>{contentToShow}</p>
-      <button>Leer mas</button>
+    <div className='flex flex-col items-center justify-between h-full p-3'>
+      <h1 className='text-lg font-semibold text-center w-full pb-2 border-b-2 border-black'>{title}</h1>
+      <p className='font-medium text-sm p-2 pb-4'>{contentToShow}</p>
+      <button 
+        type='button' 
+        className='w-min whitespace-nowrap focus:outline-none border-green-600 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>
+          Leer mas
+      </button>
     </div>
-    
   </article>
 )
