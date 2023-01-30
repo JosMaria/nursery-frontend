849c0cbc5a70bc496149c098b0724ab595426440
Jose Maria <josmariaguilar@gmail.com>
2023-01-30 00:54:23 -0400

    adding alert

--- src/pages/content/NewsPage.tsx
+++ src/pages/content/NewsPage.tsx
@@ -5,6 +5,7 @@ import newsImage from '../../assets/news-picture.png'
 export const NewsPage = () => {
   return (
     <div className='flex flex-col items-center bg-green-700'>
+      <Alert />
       {
         myNews.map(news => 
           <News 
@@ -14,7 +15,7 @@ export const NewsPage = () => {
             contentToShow={news.contentToShow}
             content={news.content}
             urlImage={news.urlImage}
-          />)        
+          />)
       }
     </div>
   )
@@ -36,3 +37,26 @@ const News = ({ title, contentToShow, content, urlImage }: NewsResponseDTO) => (
     </div>
   </article>
 )
+
+const Alert = () => (
+  <div id="alert-3" className="self-start flex p-4 my-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+    <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+    </svg>
+    <span className="sr-only">Info</span>
+    <div className="mx-3 text-sm font-medium">
+      A simple info alert with an. Give it a click if you like.
+    </div>
+    <button 
+      type='button'
+      className="ml-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" 
+      data-dismiss-target="#alert-3" 
+      aria-label="Close"
+    >
+      <span className="sr-only">Close</span>
+      <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+      </svg>
+    </button>
+  </div>
+);
