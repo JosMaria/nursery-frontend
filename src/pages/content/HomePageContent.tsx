96d7bbbb8d0016f1f7c7b0dd77d2868b5f3a81df
JoseMaria <josmariaguilar@gmail.com>
2023-03-27 16:35:14 -0400

    add div empty to page without plants

--- src/pages/content/HomePageContent.tsx
+++ src/pages/content/HomePageContent.tsx
@@ -2,10 +2,7 @@ import { useEffect, useState } from 'react';
 import { Product, ClassificationNavbar } from '../../components'
 import { fetchAllProducts, fetchAllProductsByClassification } from '../../services';
 import { ProductResponseDTO } from '../../types';
-import { GrFormPreviousLink, GrFormNextLink } from "react-icons/gr";
-import { fetchAllClassificationsByUtility } from '../../services/newsService';
 
-const totalProductsByPage = 12;
 const FIRST_PAGE = 0;
 
 const styleButtonPage = 'gap-1 text-lg font-medium bg-slate-300 hover:bg-slate-400 text-gray-900 p-4 rounded-xl';
@@ -67,65 +64,19 @@ interface ContentProductsProps {
 const ContentProducts = ({ products }: ContentProductsProps) => (
   <section className='flex flex-wrap justify-evenly gap-10 p-8'>
     {
-      products.map(product =>
-        <Product
-          key={product.id}
-          id={product.id}
-          commonName={product.commonName}
-          scientificName={product.scientificName}
-          firstLetterLastname={product.firstLetterLastname}
-          family={product.family}
-          status={product.status}
-          urlPicture={product.urlPicture}
-        />)
+      products.length === 0 ? <div>No hay plantas</div> :
+        products.map(product =>
+          <Product
+            key={product.id}
+            id={product.id}
+            commonName={product.commonName}
+            scientificName={product.scientificName}
+            firstLetterLastname={product.firstLetterLastname}
+            family={product.family}
+            status={product.status}
+            urlPicture={product.urlPicture}
+          />
+        )
     }
   </section>
 )
-
-interface SectionPageButtonProps {
-  page: number,
-  productsLength: number
-  prevPage: () => void,
-  nextPage: () => void
-}
-
-const SectionPageButton = ({ page, productsLength, prevPage, nextPage }: SectionPageButtonProps) => (
-  <section className='flex gap-10 justify-center p-7 w-full'>
-    <PageButton
-      changePage={prevPage}
-      disabled={page < 0}
-      icon={<GrFormPreviousLink size='1.4em' />}
-      reverse={true}
-      text='Anterior'
-    />
-    <PageButton
-      changePage={nextPage}
-      disabled={productsLength === 0}
-      icon={<GrFormNextLink size='1.4em' />}
-      reverse={false}
-      text='Siguiente'
-    />
-  </section>
-)
-
-interface PageButtonProps {
-  changePage: () => void,
-  disabled: boolean,
-  icon: JSX.Element,
-  reverse: boolean,
-  text: string
-}
-
-const PageButton = ({ changePage, disabled, icon, text, reverse }: PageButtonProps) => {
-  const style = 'flex items-center gap-1 text-lg font-medium bg-slate-300 hover:bg-slate-400 text-gray-900 p-4 rounded-xl'
-  return (
-    <button
-      className={reverse ? `${style} flex-row-reverse` : `${style}`}
-      onClick={changePage}
-      disabled={disabled}
-    >
-      {text}
-      {icon}
-    </button>
-  )
-}
