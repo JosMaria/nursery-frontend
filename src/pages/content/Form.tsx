2dc004e58fd50b9ba85f480b67841e676f0040ad
JoseMaria <josmariaguilar@gmail.com>
2023-04-13 11:45:21 -0400

    traslate to other english

--- src/pages/content/Form.tsx
+++ src/pages/content/Form.tsx
@@ -1,15 +1,25 @@
-import { FieldValues, useForm, UseFormRegister } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import { Status, Classification } from '../../types/product'
 
 const STYLE_LABEL = 'pb-2 pl-2 text-sm font-semibold text-gray-900 dark:text-white'
 
 // TODO: delete this data
 const options = ['familia 1', 'familia 2', 'familia 3', 'familia 4', 'familia 5', 'familia 6']
-const states = ['DISPONIBLE', 'CONSERVACIÓN', 'NO EXISTENTE']
-const classifications = ['ORNAMENTAL', 'MEDICINAL', 'INDUSTRIAL']
+const states: Array<StatusSpanish> = ['DISPONIBLE', 'CONSERVACION', 'NO EXISTENTE']
+const classifications: Array<Classification> = ['cactus', 'crasa', 'exotica', 'frutal', 'forestal', 'alimenticia', 'industrial', 'medicinal', 'ornamental', 'suculental']
+
+interface IFormInput {
+  commonName: string;
+  scientificName: string;
+  firstLetterLastname: string;
+  family: string;
+  status: string;
+  classifications: Array<string>;
+}
 
 export const Form = () => {
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<IFormInput>();
 
   const clickSubmit = (values: FieldValues) => {
     console.log('values: ', values)
@@ -76,19 +86,19 @@ export const Form = () => {
         </div>
 
         <div className='flex flex-col w-full'>
-          <label className={STYLE_LABEL} htmlFor='state'>
+          <label className={STYLE_LABEL} htmlFor='status'>
             Estado
           </label>
           <select
             className='w-full p-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            id='state'
-            {...register('state')}>
+            id='status'
+            {...register('status')}>
             {
               states.map((optionItem, index) =>
                 <option
                   className='p-3'
                   key={index}
-                  value={optionItem}>
+                  value={traslateStatus(optionItem)}>
                   {optionItem}
                 </option>)
             }
@@ -115,37 +125,16 @@ export const Form = () => {
   )
 }
 
-const FormOptionsMultiple = () => {
-  return (
-    <div className='w-2/3'>
-      <label className={STYLE_LABEL}>Clasificaci&oacute;n</label>
-      <ul className='w-full p-1 m-1 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white'>
-        
-        <li className='border-b border-gray-200 rounded-t-lg dark:border-gray-600'>
-          <div className='flex items-center p-2'>
-            <input id='item-medicinal' type='checkbox' value='medicinal' className='mr-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' />
-            <label htmlFor='item-medicinal' className='text-sm font-medium text-gray-900 dark:text-gray-300'>
-              MEDICINAL
-            </label>
-          </div>
-        </li>
-        <li className='border-b border-gray-200 rounded-t-lg dark:border-gray-600'>
-          <div className='flex items-center p-2'>
-            <input id='item-ornamental' type='checkbox' value='ornamental' className='mr-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' />
-            <label htmlFor='item-ornamental' className='text-sm font-medium text-gray-900 dark:text-gray-300'>
-              ORNAMENTAL
-            </label>
-          </div>
-        </li>
-        <li className='border-b border-gray-200 rounded-t-lg dark:border-gray-600'>
-          <div className='flex items-center p-2'>
-            <input id='item-industrial' type='checkbox' value='industrial' className='mr-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' />
-            <label htmlFor='item-industrial' className='text-sm font-medium text-gray-900 dark:text-gray-300'>
-              INDUSTRIAL
-            </label>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+// NOTE: all dtos in english
+type StatusSpanish = 'DISPONIBLE' | 'CONSERVACION' | 'NO EXISTENTE';
+
+
+const traslateStatus = (state: StatusSpanish): Status => {
+  if (state === 'DISPONIBLE') {
+    return 'AVAILABLE';
+  } else if (state === "CONSERVACION") {
+    return 'IN_CONSERVATION';
+  } else {
+    return 'NON_EXISTENT';
+  }
 }
