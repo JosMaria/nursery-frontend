fe12d691505b6bbe5c5deb1fdd8555322d881576
JoseMaria <josmariaguilar@gmail.com>
2023-04-28 12:16:18 -0400

    add fetch all products and fetch by id products in services

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,12 +1,18 @@
-import axios from "axios"
-import { CreatePlantDTO, IdentificationResponseDTO, SingleProductResponseDTO } from "../types";
+import { ProductResponseDTO } from '../types/dto'
+import { createInstance } from "./http";
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8080'
-})
+/**
+ * Axios instance that will allow requests to be made to the API related to the News.
+ */
+const productService = createInstance({ instanceURL: '/products' });
 
-export const fetchAllProducts = async (numberPage = 0) => {
-  const { data } = await instance.get('/api/v1/plants', {
+/**
+ * Function that allows you to fetch all products.
+ * @param numberPage contains the number of the page.
+ * @returns { Promise<Array<import('../types').ProductResponseDTO>> } all products.
+ */
+export const fetchAllProducts = async (numberPage = 0): Promise<Array<ProductResponseDTO>> => {
+  const { data } = await productService.get('/', {
     params: {
       page: numberPage
     }
@@ -14,32 +20,37 @@ export const fetchAllProducts = async (numberPage = 0) => {
   return data;
 }
 
-export const fetchProductById = async (id: string): Promise<SingleProductResponseDTO> => {
-  const { data } = await instance.get(`/api/v1/plants/${id}`);
-  return data;
-};
-
-export const fetchAllProductsByClassification = async (classification: string) => {
-  const { data } = await instance.get(`/api/v1/plants/types/${classification}`);
-  return data;
-}
-
-export const fetchAllIdentification = async (): Promise<Array<IdentificationResponseDTO>> => {
-  const { data } = await instance.get(`/api/v1/plants/identifications`);
+/**
+ * Function that allows you to fetch product by id.
+ * @param productId contains the product's id.
+ * @returns { Promise<import('../types').ProductResponseDTO> } product.
+ */
+export const fetchByIdProduct = async (productId: number): Promise<ProductResponseDTO> => {
+  const { data } = await productService.get(`/${productId}`);
   return data;
 }
 
-export const getUrls = async (count = 5): Promise<Array<string>> => {
-  const response: Array<string> = [];
+// export const fetchAllProductsByClassification = async (classification: string) => {
+//   const { data } = await instance.get(`/api/v1/plants/types/${classification}`);
+//   return data;
+// }
+
+// export const fetchAllIdentification = async (): Promise<Array<IdentificationResponseDTO>> => {
+//   const { data } = await instance.get(`/api/v1/plants/identifications`);
+//   return data;
+// }
+
+// export const getUrls = async (count = 5): Promise<Array<string>> => {
+//   const response: Array<string> = [];
   
-  for (let i = 0; i < count; i++) {
-      const { data } = await axios.get('https://dog.ceo/api/breeds/image/random');    
-      response.push(data.message);
-  }
-  return response;
-}
+//   for (let i = 0; i < count; i++) {
+//       const { data } = await axios.get('https://dog.ceo/api/breeds/image/random');    
+//       response.push(data.message);
+//   }
+//   return response;
+// }
 
-export const createPlant = async (createPlantDTO: CreatePlantDTO) => {
-  const { data } = await instance.post(`/api/v1/plants`, createPlantDTO)
-  return data;
-}
+// export const createPlant = async (createPlantDTO: CreatePlantDTO) => {
+//   const { data } = await instance.post(`/api/v1/plants`, createPlantDTO)
+//   return data;
+// }
---
+++ src/types/dto/product.d.ts
@@ -0,0 +1,9 @@
+export interface ProductResponseDTO {
+    id: number
+    commonName: string
+    scientificName: string
+    scientistSurnameInitial: string
+    status: string
+    family: string
+    firstUrlPicture: string
+}
