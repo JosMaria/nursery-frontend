580492a9725cc70ed816708d53366f40e2ea5aab
JoseMaria <josmariaguilar@gmail.com>
2023-04-30 19:58:33 -0400

    add form create plant, and fetch all families

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ListPage, NewsPage, CatalogPage, SingleNews, SingleProduct } from '../pages';
+import { ListPage, NewsPage, CatalogPage, SingleNews, SingleProduct, CreatePlantForm } from '../pages';
 import { Page } from '../pages/HomePage';
 
 export const AppRoute = () => (
@@ -11,6 +11,7 @@ export const AppRoute = () => (
         <Route path='list' element={<ListPage />} />
         <Route path='news' element={<NewsPage />} />
         <Route path='news/:newsId' element={<SingleNews />} />
+        <Route path='form-create' element={<CreatePlantForm />} />
       </Route>
     </Routes>
   </BrowserRouter>
--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export * from './newsService';
 export * from './plantService';
 export * from './classificationService';
 export * from './imageService';
+export * from './familyService'
