c83f8e01b653246285accfa53d19bb91f34b4664
JoseMaria <josmariaguilar@gmail.com>
2023-05-09 18:53:39 -0400

    add styles to the form

--- src/pages/create-plant-form/CreatePlantForm.tsx
+++ src/pages/create-plant-form/CreatePlantForm.tsx
@@ -3,6 +3,8 @@ import { CreatePlantDTO } from '../../types';
 import { createPlant, fetchAllFamilies } from '../../services';
 import { useEffect, useState } from 'react';
 
+const classifications = ['ORNAMENTAL', 'FOREST', 'INDUSTRIAL', 'ALIMENTARY', 'MEDICINAL', 'EXOTIC', 'CACTUS', 'FRUITFUL', 'GRASS', 'SUCCULENT'];
+
 export const CreatePlantForm = () => {
   const { register, handleSubmit, reset } = useForm<CreatePlantDTO>();
   const [families, setFamilies] = useState<Array<string>>([]);
@@ -20,15 +22,15 @@ export const CreatePlantForm = () => {
 
   return (
     <main className='bg-[var(--color-level-four)] w-full flex flex-col items-center'>
-      
+
       <form
-        className='bg-[var(--color-level-two)] w-1/2 grid grid-cols-2 gap-4 justify-items-center p-5'
+        className='bg-[var(--color-level-two)] w-1/2 grid grid-cols-2 gap-4 justify-items-center p-5 m-5'
         onSubmit={handleSubmit(clickSubmit)}>
-      
-        <h1 className='col-span-2 text-[var(--color-level-six)] font-medium text-2xl'>CREAR PLANTA</h1>
+
+        <h1 className='col-span-2 text-[var(--color-level-six)] font-semibold text-2xl'>CREAR PLANTA</h1>
 
         <div className='flex flex-col gap-y-1 w-4/5 p-3'>
-          <label className='text-(var(--color-level-six)) text-sm font-medium pl-1' htmlFor='commonName'>
+          <label className='text-(var(--color-level-six)) text-base font-semibold pl-1' htmlFor='commonName'>
             Nombre Com&uacute;n
           </label>
           <input
@@ -39,7 +41,7 @@ export const CreatePlantForm = () => {
         </div>
 
         <div className='flex flex-col gap-y-1 w-full p-3 justify-self-start'>
-          <label className='text-(var(--color-level-six)) text-sm font-medium pl-1' htmlFor='scientificName'>
+          <label className='text-(var(--color-level-six)) text-base font-semibold pl-1' htmlFor='scientificName'>
             Nombre Cientifico
           </label>
           <div className='flex gap-5'>
@@ -56,8 +58,8 @@ export const CreatePlantForm = () => {
           </div>
         </div>
 
-        <div className='flex flex-col gap-y-1 w-1/2'>
-          <label className='text-(var(--color-level-six)) text-sm font-medium pl-1'  htmlFor='family'>
+        <div className='flex flex-col gap-y-1 p-3 w-1/2'>
+          <label className='text-(var(--color-level-six)) text-base font-semibold pl-1' htmlFor='family'>
             Familia
           </label>
           <select className='bg-[var(--color-level-one)] p-3 text-sm rounded'
@@ -71,7 +73,49 @@ export const CreatePlantForm = () => {
             }
           </select>
         </div>
-    </form>
+
+        <div className='flex flex-col gap-y-1 w-1/2 p-3 justify-self-start'>
+          <label className='text-(var(--color-level-six)) text-base font-semibold pl-1' htmlFor='status'>
+            Estado
+          </label>
+          <select className='bg-[var(--color-level-one)] p-3 text-sm rounded'
+            id='status'
+            {...register('status')}>
+            {
+              ['AVAILABLE', 'NON_EXISTENT', 'CONSERVATION'].map((optionItem, index) =>
+                <option className='p-3' key={index} value={optionItem}>
+                  {optionItem}
+                </option>)
+            }
+          </select>
+        </div>
+
+        <div className="col-span-2 flex flex-col gap-y-2 p-3">
+          <label className='text-(var(--color-level-six)) text-base font-semibold pl-1'>
+            Clasificaciones
+          </label>
+          <div className="flex flex-wrap gap-3 justify-center">
+            {
+              classifications.map((value, index) =>
+                <div key={index} className='bg-[var(--color-level-four)] w-36 p-3 rounded-md'>
+                  <input className="mr-2" type='checkbox' id={value} value={value} {...register('classifications')} />
+                  <label className="text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor={value}>
+                    {value}
+                  </label>
+                </div>
+              )
+            }
+          </div>
+        </div>
+
+        <div className="col-span-2 flex justify-center m-5">
+          <button
+            className='text-white font-medium bg-[var(--color-level-five)] hover:bg-[var(--color-level-four)] rounded text-base py-4 px-7'
+            type='submit'>
+            Crear planta
+          </button>
+        </div>
+      </form>
     </main>
   )
 }
