85898c004e19097a08f0a1d2522dd8892334aff6
JoseMaria <josmariaguilar@gmail.com>
2023-05-09 11:22:16 -0400

    add styles

--- src/pages/create-plant-form/CreatePlantForm.tsx
+++ src/pages/create-plant-form/CreatePlantForm.tsx
@@ -3,8 +3,6 @@ import { CreatePlantDTO } from '../../types';
 import { createPlant, fetchAllFamilies } from '../../services';
 import { useEffect, useState } from 'react';
 
-const STYLE_LABEL = 'pb-2 pl-2 text-sm font-semibold text-gray-900 dark:text-white'
-
 export const CreatePlantForm = () => {
   const { register, handleSubmit, reset } = useForm<CreatePlantDTO>();
   const [families, setFamilies] = useState<Array<string>>([]);
@@ -21,57 +19,59 @@ export const CreatePlantForm = () => {
   }
 
   return (
-    <form
-      className='w-1/2 p-5 m-5 bg-slate-600 flex flex-wrap justify-start gap-y-5 gap-x-10'
-      onSubmit={handleSubmit(clickSubmit)}>
-      <h1 className='w-full pb-1 text-2xl text-center dark:text-white font-semibold'>CREAR PLANTA</h1>
+    <main className='bg-[var(--color-level-four)] w-full flex flex-col items-center'>
+      
+      <form
+        className='bg-[var(--color-level-two)] w-1/2 grid grid-cols-2 gap-4 justify-items-center p-5'
+        onSubmit={handleSubmit(clickSubmit)}>
+      
+        <h1 className='col-span-2 text-[var(--color-level-six)] font-medium text-2xl'>CREAR PLANTA</h1>
 
-      <div className='flex flex-col w-1/3'>
-        <label htmlFor='commonName' className={STYLE_LABEL}>
-          Nombre Com&uacute;n
-        </label>
-        <input
-          className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
-          type='text' id='commonName' required
-          {...register('commonName')}
-        />
-      </div>
-
-      <div className='flex flex-col w-1/2'>
-        <label className={STYLE_LABEL} htmlFor='scientificName'>
-          Nombre Cientifico
-        </label>
-        <div className='flex gap-5'>
+        <div className='flex flex-col gap-y-1 w-4/5 p-3'>
+          <label className='text-(var(--color-level-six)) text-sm font-medium pl-1' htmlFor='commonName'>
+            Nombre Com&uacute;n
+          </label>
           <input
-            className='w-9/12 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
-            type='text' id='scientificName'
-            {...register('scientificName')}
-
-          />
-          <input
-            className='w-1/6 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
-            type='text'
-            {...register('scientistSurnameInitial')}
+            className='bg-[var(--color-level-one)] p-3 text-sm rounded'
+            type='text' id='commonName' required
+            {...register('commonName')}
           />
         </div>
-      </div>
 
-      <div className='flex flex-col w-1/3'>
-        <label className={STYLE_LABEL} htmlFor='family'>
-          Familia
-        </label>
-        <select
-          className='w-full p-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-          id='family'
-          {...register('family')}>
-          {
-            families.map((optionItem, index) =>
-              <option className='p-3' key={index} value={optionItem}>
-                {optionItem}
-              </option>)
-          }
-        </select>
-      </div>
+        <div className='flex flex-col gap-y-1 w-full p-3 justify-self-start'>
+          <label className='text-(var(--color-level-six)) text-sm font-medium pl-1' htmlFor='scientificName'>
+            Nombre Cientifico
+          </label>
+          <div className='flex gap-5'>
+            <input
+              className='bg-[var(--color-level-one)] p-3 text-sm rounded w-4/6'
+              type='text' id='scientificName'
+              {...register('scientificName')}
+            />
+            <input
+              className='bg-[var(--color-level-one)] p-3 text-sm rounded w-1/6'
+              type='text'
+              {...register('scientistSurnameInitial')}
+            />
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-y-1 w-1/2'>
+          <label className='text-(var(--color-level-six)) text-sm font-medium pl-1'  htmlFor='family'>
+            Familia
+          </label>
+          <select className='bg-[var(--color-level-one)] p-3 text-sm rounded'
+            id='family'
+            {...register('family')}>
+            {
+              families.map((optionItem, index) =>
+                <option className='p-3' key={index} value={optionItem}>
+                  {optionItem}
+                </option>)
+            }
+          </select>
+        </div>
     </form>
+    </main>
   )
 }
