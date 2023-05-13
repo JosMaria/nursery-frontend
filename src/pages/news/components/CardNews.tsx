b13879cd05a120825a4f48ee7999644fb5301ec4
JoseMaria <josmariaguilar@gmail.com>
2023-05-12 20:39:36 -0400

    fix styles to the single news page

--- src/pages/news/components/CardNews.tsx
+++ src/pages/news/components/CardNews.tsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom'
 import { NewsResponseDTO } from '../../../types'
 
 export const CardNews = ({ id, title, description, urlImage }: NewsResponseDTO) => (
 	<div className='w-1/2 flex rounded shadow bg-[var(--color-level-six)] hover:shadow-lg'>
 		{
-			urlImage && 
-				<img
-					className='pl-3 h-fit py-3 object-cover rounded-t-2xl md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
-					src='https://www.aboutespanol.com/thmb/oxcVdVmVah-wUyZJVxr_OXn43qc=/3771x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-141249909-597c5a013df78cbb7a279e0a.jpg'
-					alt={title}
-				/>
+			urlImage &&
+			<img
+				className='pl-3 h-fit py-3 object-cover rounded-t-2xl md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
+				src='https://www.aboutespanol.com/thmb/oxcVdVmVah-wUyZJVxr_OXn43qc=/3771x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-141249909-597c5a013df78cbb7a279e0a.jpg'
+				alt={title}
+			/>
 		}
 		<Preview
 			id={id}
@@ -19,16 +20,18 @@ export const CardNews = ({ id, title, description, urlImage }: NewsResponseDTO) 
 	</div>
 )
 
-const Preview = ({ title, description }: NewsResponseDTO) => (
+const Preview = ({ id, title, description }: NewsResponseDTO) => (
 	<div className='w-full flex flex-col justify-between gap-2 p-4'>
 		<div>
 			<h5 className='text-lg mb-2 first-letter:capitalize font-bold tracking-tight text-[var(--color-level-one)]'>{title}</h5>
 			<p className='font-normal first-letter:capitalize text-sm text-[var(--color-level-one)]'>{description.slice(0, 250)}...</p>
 		</div>
-		<button
-			className='self-end w-fit px-8 py-1.5 rounded text-white font-medium text-base bg-[var(--color-level-three)] hover:bg-[var(--color-level-four)]'
-			type='button' >
-			Ver mas
-		</button>
+		<Link to={`/news/${id}`} className='self-end'>
+			<button
+				className='w-fit px-8 py-2 rounded text-white font-medium text-base bg-[var(--color-level-three)] hover:bg-[var(--color-level-four)]'
+				type='button' >
+				Ver mas
+			</button>
+		</Link>
 	</div>
 )
--- src/pages/single_news/SingleNews.tsx
+++ src/pages/single_news/SingleNews.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { NewsResponseDTO } from '../../types'
 import { useParams } from 'react-router';
 import { fetchByIdNews } from '../../services';
+import { Link } from 'react-router-dom';
 
 const VALUE_INITIAL: NewsResponseDTO = {
   id: 0,
@@ -11,7 +12,7 @@ const VALUE_INITIAL: NewsResponseDTO = {
 }
 
 export const SingleNews = () => {
-  const [singleNews, setSingleNews] = useState<NewsResponseDTO>(VALUE_INITIAL);
+  const [singleNews, setSingleNews] = useState(VALUE_INITIAL);
   const { newsId } = useParams();
 
   useEffect(() => {
@@ -19,28 +20,32 @@ export const SingleNews = () => {
       fetchByIdNews(parseInt(newsId))
         .then(responseNews => setSingleNews(responseNews));
     }
-  }, [])
+  }, []);
 
   return (
-    <div className='flex flex-col items-center gap-5 h-full px-40 py-10 leading-normal'>
-      <h1 className='text-6xl text-center first-letter:capitalize font-bold tracking-tight text-gray-900 dark:text-black'>
-          {singleNews.title}
+    <div className='flex flex-col items-center gap-5 bg-[var(--color-level-two)] w-2/3 p-5'>
+      <h1 className='text-4xl text-center first-letter:capitalize font-semibold'>
+        {singleNews.title}
       </h1>
-        {
-          singleNews.urlImage && (
-            <img
-                className='pl-3 h-fit py-3 object-cover rounded-t-2xl md:h-auto md:w-1/3 md:rounded-none md:rounded-l-lg'
-                src='https://dog.ceo/api/breeds/image/random'
-                alt={singleNews.title}
-            />
-          )
-        }
-      <p className='text-lg font-normal first-letter:capitalize text-gray-700 dark:text-black'>
+      {
+        singleNews.urlImage && (
+          <img
+            className='w-1/3'
+            src='https://www.aboutespanol.com/thmb/oxcVdVmVah-wUyZJVxr_OXn43qc=/3771x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-141249909-597c5a013df78cbb7a279e0a.jpg'
+            alt={singleNews.title}
+          />
+        )
+      }
+      <p className='text-base first-letter:capitalize text-black'>
         {singleNews.description}
-      </p>    
-      <button type='button' className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>
+      </p>
+      <Link to='/news'>
+        <button
+          className='w-fit px-8 py-2 rounded text-white font-medium text-base bg-[var(--color-level-three)] hover:bg-[var(--color-level-four)]'
+          type='button'>
           Volver
-      </button>
+        </button>
+      </Link>
     </div>
   )
 }
