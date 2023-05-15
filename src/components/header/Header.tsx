a96b42c764a38fce7efd042c1e7c6f86c12d85c6
JoseMaria <josmariaguilar@gmail.com>
2023-05-15 16:36:22 -0400

    fix style of the navbar in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import { FaUserCircle } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 
 const style = 'text-lg hover:bg-[var(--color-level-five)] text-[var(--color-level-one)] p-3 font-medium';
+const styleSelected = 'border-b-4 border-[var(--color-level-one)]';
 
 export const Header = () => (
   <header className='bg-[var(--color-level-seven)] flex justify-between items-center px-10 py-6'>
@@ -22,17 +23,17 @@ const Logo = () => (
 
 const Navbar = () => (
   <nav>
-      <NavLink className={({ isActive }) => isActive ? `${style} dark:text-blue-500` : `${style} dark:text-white`}
+      <NavLink className={({ isActive }) => isActive ? `${style} ${styleSelected}` : `${style}`}
         to={'/'}>
         Inicio
       </NavLink>
 
-      <NavLink className={({ isActive }) => isActive ? `${style} dark:text-blue-500` : `${style} dark:text-white`}
+      <NavLink className={({ isActive }) => isActive ? `${style} ${styleSelected}` : `${style}`}
         to={'/list'}>
         Listado
       </NavLink>
 
-      <NavLink className={({ isActive }) => isActive ? `${style} dark:text-blue-500` : `${style} dark:text-white`}
+      <NavLink className={({ isActive }) => isActive ? `${style} ${styleSelected}` : `${style}`}
         to={'/news'}>
         Novedades
       </NavLink>
