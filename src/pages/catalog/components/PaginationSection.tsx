523526b373c079441c4db42df65fd6520994e61e
JoseMaria <josmariaguilar@gmail.com>
2023-06-27 15:37:28 -0400

    implemented section of pagination

--- src/pages/catalog/components/PaginationSection.tsx
+++ src/pages/catalog/components/PaginationSection.tsx
@@ -4,38 +4,56 @@ import { BiArrowToLeft } from 'react-icons/bi'
 
 export const PaginationSection = () => {
   const { catalog: { page }, firstPage, nextPage, prevPage } = useCatalogContext()
-  const { first: isFirst, last: isLast, number } = page
+  const {
+    first: isFirst,
+    last: isLast,
+    number,
+    totalPages,
+    size,
+    numberOfElements,
+    totalElements
+  } = page
 
   return (
-    <section className='flex justify-around'>
-      <PagingButton
-        style='flex text-[var(--color-level-one)]'
-        isDisabled={number < 1}
-        functionality={firstPage}
-      >
-        <p className='font-medium'>&nbsp;Ir a la p&aacute;gina 1</p>
-        <BiArrowToLeft size={'1.5em'} color='var(--color-level-one)' />
-      </PagingButton>
-
-      <div className='flex gap-5'>
+    <section>
+      <PagingText
+        start={(number * size) + 1}
+        end={(number * size) + numberOfElements}
+        total={totalElements}
+      />
+      <div className='grid grid-cols-3 place-items-center gap-5 pb-3'>
         <PagingButton
-          style='font-medium flex items-center gap-3 py-2 px-5 rounded-lg border-[0.2em] border-[var(--color-level-six)] bg-[var(--color-level-two)] hover:bg-[var(--color-level-three)]'
-          isDisabled={isFirst}
-          functionality={prevPage}
+          style='flex text-[var(--color-level-one)]'
+          isDisabled={number < 1}
+          functionality={firstPage}
         >
-          <HiOutlineArrowLongLeft size={'1.5em'} color='black' />
+          <BiArrowToLeft size={'1.5em'} color='var(--color-level-one)' />
+          <p className='font-medium'>&nbsp;Ir a la p&aacute;gina 1</p>
         </PagingButton>
 
-        <PagingButton
-          style='font-medium flex items-center gap-3 py-2 px-5 rounded-lg text-[var(--color-level-one)] bg-[var(--color-level-seven)] hover:bg-[var(--color-level-six)]'
-          isDisabled={isLast}
-          functionality={nextPage}
-        >
-          <p>Siguiente</p>
-          <HiOutlineArrowLongRight size={'1.5em'} color='var(--color-level-one)' />
-        </PagingButton>
+        <div className='flex gap-5'>
+          <PagingButton
+            style='font-medium flex items-center gap-3 py-2 px-5 rounded-lg border-[0.2em] border-[var(--color-level-six)] bg-[var(--color-level-two)] hover:bg-[var(--color-level-three)]'
+            isDisabled={isFirst}
+            functionality={prevPage}
+          >
+            <HiOutlineArrowLongLeft size={'1.5em'} color='black' />
+          </PagingButton>
+
+          <PagingButton
+            style='font-medium flex items-center gap-3 py-2 px-5 rounded-lg text-[var(--color-level-one)] bg-[var(--color-level-seven)] hover:bg-[var(--color-level-six)]'
+            isDisabled={isLast}
+            functionality={nextPage}
+          >
+            <p>Siguiente</p>
+            <HiOutlineArrowLongRight size={'1.5em'} color='var(--color-level-one)' />
+          </PagingButton>
+        </div>
+
+        <PagingInfo numberPage={number} totalPages={totalPages} />
       </div>
     </section>
+
   )
 }
 
@@ -54,4 +72,30 @@ const PagingButton = ({ children, style, isDisabled, functionality }: PagingButt
   >
     {children}
   </button>
+)
+
+interface PagingTextProps {
+  start: number
+  end: number
+  total: number
+}
+
+const PagingText = ({ start, end, total }: PagingTextProps) => (
+  <p className='ml-10 text-base font-light text-[var(--color-level-one)]'>
+    Mostrando: <b className='font-semibold'>{start} - {end}</b> de <b className='font-semibold'>{total}</b>
+  </p>
+)
+
+interface PagingInfoProps {
+  numberPage: number
+  totalPages: number
+}
+
+const PagingInfo = ({ numberPage, totalPages }: PagingInfoProps) => (
+  <p className='flex text-[var(--color-level-one)] font-light'>
+    P&aacute;gina
+    <b className='font-semibold'>&nbsp;{numberPage + 1}</b>
+    &nbsp;de&nbsp;
+    <b className='font-semibold'>{totalPages}</b>
+  </p>
 )
